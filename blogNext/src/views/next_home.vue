3ae87a3cea6b49a2efc61e65c40d938b0ead6467
landers <liaorenj@gmail.com>
2023-09-11 23:26:27 +0800

    update 更新UI样式 感谢朋友提的一些改进建议 让Blog NEXT更好

--- blogNext/src/views/next_home.vue
+++ blogNext/src/views/next_home.vue
@@ -12,24 +12,26 @@
           <el-menu-item index="2-4"><a href="/message">留言</a></el-menu-item>
           <el-submenu index="2-5" :popper-append-to-body="false">
             <template slot="title">管理</template>
-            <el-menu-item index="2-5-1">
+            <el-menu-item index="2-5-1"><a href="/login">登录</a></el-menu-item>
+            <el-menu-item index="2-5-2">
               <a href="/overview">架构总览</a>
             </el-menu-item>
-            <el-menu-item index="2-5-2">
+            <el-menu-item index="2-5-3">
               <a href="/dashboard">后台面板</a>
             </el-menu-item>
           </el-submenu>
         </el-submenu>
         <el-menu-item index="3"><a href="/zhuanlan">专栏</a></el-menu-item>
         <el-menu-item index="4"><a href="/about">关于</a></el-menu-item>
-        <el-menu-item index="4"><a href="/login">登录</a></el-menu-item>
+        <el-menu-item index="5"><a href="/new" target="_blank">What's new</a></el-menu-item>
         <el-menu-item index="5"><a :href="custom.github_url" target="_blank">Github</a></el-menu-item>
         <el-menu-item index="5"><a :href="custom.project_url" target="_blank">Mgek APP</a></el-menu-item>
         <el-menu-item index="5"><a :href="custom.jjapplication_url" target="_blank">JJAPP</a></el-menu-item>
-        <el-menu-item index="5"><a href="/new" target="_blank">What's new</a></el-menu-item>
       </el-menu>
     </div>
-    <div class="next-banner">
+    <div style="position:relative;height: 100%;width: 100%">
+      <div class="next-banner">
+      </div>
       <div class="next-body">
         <h1 class="animated fadeInDown" @click="explore">
           Welcome to Blog
@@ -37,7 +39,7 @@
         </h1>
         <p
             class="animated fadeInDown"
-          style="
+            style="
             text-align: left;
             color: #ffbb5b;
             margin: 1rem 0;
@@ -95,7 +97,7 @@ export default {
 }
 .next-header .el-menu {
   height: 3rem;
-  background-color: #100e0e3b;
+  background-color: #100e0e85;
 }
 #next-home /deep/ .el-menu.el-menu--horizontal {
   border-bottom: none;
@@ -113,12 +115,12 @@ export default {
 #next-home /deep/ .el-menu--horizontal > .el-menu-item:not(.is-disabled):focus,
 #next-home /deep/ .el-menu--horizontal > .el-menu-item:not(.is-disabled):hover,
 #next-home /deep/ .el-menu--horizontal > .el-submenu .el-submenu__title:hover {
-  background-color: #100e0e3b;
+  background-color: #100e0e85;
 }
 
 #next-home /deep/ .el-menu--horizontal .el-menu .el-menu-item,
 #next-home /deep/ .el-menu--horizontal .el-menu .el-submenu__title {
-  background-color: #100e0e3b;
+  background-color: #100e0e85;
   color: #ffffff;
 }
 
@@ -144,13 +146,14 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  z-index: 1;
   background-image: url('../assets/background.webp');
   width: 100%;
   height: 100%;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  opacity: 0.85;
+  filter: blur(0);
 }
 .next-body {
   position: fixed;
